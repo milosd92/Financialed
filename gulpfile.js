f4c2e0ebb1908d51ceb3aa1f1f9d00ea69e1403c
milosd92 <milosdanilov@gmail.com>
2016-10-13 22:32:46 +0200

    Creating template for site

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,26 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
 gulp.task('styles', function () {
+
+    // Bootstrap
+    gulp.src([
+        './web/bower_components/gentelella/vendors/bootstrap/dist/css/bootstrap.css'
+    ]).pipe(concat('bootstrap.css')).pipe(gulp.dest('./web/assets/vendor/css'));
+
+    // Font-Awesome
+    gulp.src([
+        './web/bower_components/gentelella/vendors/font-awesome/css/font-awesome.css'
+    ]).pipe(concat('font-awesome.css')).pipe(gulp.dest('./web/assets/vendor/css'));
+
+    // Application-custom
     return gulp.src([
         './web/bower_components/gentelella/src/scss/custom.scss'
     ]).pipe(sass()).pipe(concat('gentelella-app.css')).pipe(gulp.dest('./web/assets/vendor/css'));
 });
 
 gulp.task('scripts', function () {
+
+    // Application-custom
     return gulp.src([
         './web/bower_components/gentelella/src/js/custom.js',
         './web/bower_components/gentelella/src/js/helper.js'
