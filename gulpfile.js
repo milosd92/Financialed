b9d2ac1aa1348539bc759a36801e063f1f68e297
milosd92 <milosdanilov@gmail.com>
2016-10-15 19:31:00 +0200

    extracted paths to variables

--- gulpfile.js
+++ gulpfile.js
@@ -2,54 +2,59 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
+var components_root = './web/bower_components/gentelella';
+
+var vendor_assets_src = components_root + '/vendors';
+var vendor_assets_dest = './web/assets/vendor';
+
 gulp.task('styles', function () {
 
     // Bootstrap
     gulp.src([
-        './web/bower_components/gentelella/vendors/bootstrap/dist/css/bootstrap.css'
-    ]).pipe(concat('bootstrap.css')).pipe(gulp.dest('./web/assets/vendor/css'));
+        vendor_assets_src + '/bootstrap/dist/css/bootstrap.css'
+    ]).pipe(concat('bootstrap.css')).pipe(gulp.dest(vendor_assets_dest + '/css'));
 
     // Font-Awesome
     gulp.src([
-        './web/bower_components/gentelella/vendors/font-awesome/css/font-awesome.css'
-    ]).pipe(concat('font-awesome.css')).pipe(gulp.dest('./web/assets/vendor/css'));
+        vendor_assets_src + '/font-awesome/css/font-awesome.css'
+    ]).pipe(concat('font-awesome.css')).pipe(gulp.dest(vendor_assets_dest + '/css'));
 
     // Application-custom
     return gulp.src([
-        './web/bower_components/gentelella/src/scss/custom.scss'
-    ]).pipe(sass()).pipe(concat('gentelella-app.css')).pipe(gulp.dest('./web/assets/vendor/css'));
+        components_root + '/src/scss/custom.scss'
+    ]).pipe(sass()).pipe(concat('gentelella-app.css')).pipe(gulp.dest(vendor_assets_dest + '/css'));
 });
 
 gulp.task('scripts', function () {
 
     // jQuery
     gulp.src([
-        './web/bower_components/gentelella/vendors/jquery/dist/jquery.js'
-    ]).pipe(gulp.dest('./web/assets/vendor/js'));
+        vendor_assets_src + '/jquery/dist/jquery.js'
+    ]).pipe(gulp.dest(vendor_assets_dest + '/js'));
 
     // Bootstrap
     gulp.src([
-        './web/bower_components/gentelella/vendors/bootstrap/dist/js/bootstrap.js'
-    ]).pipe(gulp.dest('./web/assets/vendor/js'));
+        vendor_assets_src + '/bootstrap/dist/js/bootstrap.js'
+    ]).pipe(gulp.dest(vendor_assets_dest + '/js'));
 
     // Application-custom
     return gulp.src([
-        './web/bower_components/gentelella/src/js/custom.js',
-        './web/bower_components/gentelella/src/js/helper.js'
-    ]).pipe(concat('gentelella-app.js')).pipe(gulp.dest('./web/assets/vendor/js'));
+        components_root + '/src/js/custom.js',
+        components_root + '/src/js/helper.js'
+    ]).pipe(concat('gentelella-app.js')).pipe(gulp.dest(vendor_assets_dest + '/js'));
 });
 
 gulp.task('fonts', function () {
 
     // Font awesome
     gulp.src([
-        './web/bower_components/gentelella/vendors/font-awesome/fonts/fontawesome-webfont.*'
-    ]).pipe(gulp.dest('./web/assets/vendor/fonts'));
+        vendor_assets_src + '/font-awesome/fonts/fontawesome-webfont.*'
+    ]).pipe(gulp.dest(vendor_assets_dest + '/fonts'));
 
     // Glyphicons
     return gulp.src([
-        './web/bower_components/gentelella/vendors/bootstrap/fonts/glyphicons-halflings-regular.*'
-    ]).pipe(gulp.dest('./web/assets/vendor/fonts'));
+        vendor_assets_src + '/bootstrap/fonts/glyphicons-halflings-regular.*'
+    ]).pipe(gulp.dest(vendor_assets_dest + '/fonts'));
 });
 
 gulp.task('default', ['styles', 'scripts', 'fonts']);
