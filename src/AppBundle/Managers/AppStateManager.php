e1a0e7498063f0f1a5fb1bd103809c690f8375ce
milosd92 <milosdanilov@gmail.com>
2016-10-18 19:59:44 +0200

    adding doc

--- src/AppBundle/Managers/AppStateManager.php
+++ src/AppBundle/Managers/AppStateManager.php
@@ -2,8 +2,21 @@
 
 namespace AppBundle\Managers;
 
+/**
+ * AppStateManager
+ *
+ * Represents the data that is avalible globaly.
+ *
+ * @package AppBundle\Managers
+ * @author Miloš Danilov <milosdanilov@gmail.com>
+ */
 class AppStateManager
 {
+    /**
+     * Returns the full name of the loged-in user.
+     *
+     * @return string
+     */
     public function getUser()
     {
         return "John Doe";
--- src/AppBundle/Twig/Extension/AppStateExtension.php
+++ src/AppBundle/Twig/Extension/AppStateExtension.php
@@ -4,15 +4,31 @@ namespace AppBundle\Twig\Extension;
 
 use AppBundle\Managers\AppStateManager;
 
+/**
+ * AppStateExtension
+ *
+ * @package AppBundle\Twig\Extension
+ * @author Miloš Danilov <milosdanilov@gmail.com>
+ */
 class AppStateExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
 {
+    /**
+     * @var \AppBundle\Managers\AppStateManager
+     */
     protected $appState;
 
+    /**
+     * AppStateExtension constructor.
+     * @param AppStateManager $appState
+     */
     public function __construct(AppStateManager $appState)
     {
         $this->appState = $appState;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getGlobals()
     {
         return array(
